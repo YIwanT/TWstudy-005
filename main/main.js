11fe904218280f901a8f1fc74ca03e49ffed5c4d
YIwanT <awansama@126.com>
2017-08-10 00:21:04 +0800

    0.3

--- main/main.js
+++ main/main.js
@@ -1,4 +1,37 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+function includes(collection, string) {
+    for (let item of collection) {
+        if (item.barcode == string){
+            return item ;
+        }
+    }
+    return null ;
+}
+function freeincludes(collection, string) {
+    for (let item of collection) {
+        for (let obj of item.barcodes) {
+            if (obj == string){
+                return string ;
+            }
+        }
+
+    }
+    return null ;
+}
+module.exports = function printInventory(inputs) {
+let result = '';
+const Datbase = require('./datbase.js');
+const AllItem = Datbase.loadAllItems();
+const Promotions = Datbase.loadPromotions();
+let  inputitem = [] ;
+let  promotionitems = [] ;
+
+    for (let item of inputs) {
+        let obj = includes(AllItem, item);
+        if(obj) {
+            inputitem.push(obj);
+        }
+        promotionitems.push(freeincludes(Promotions, item));
+    }
+    console.log(promotionitems);
+return result ;
 };
